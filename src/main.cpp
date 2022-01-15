ae164f57bf262d026b784eb4eeaf0de883f750c3
christoph-myC-beginner <christoph.schumachers@gmx.de>
2022-01-15 16:37:37 +0100

    ongoing development

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 //PORTB &= ~((1 << 0) | (1 << 3)); // löscht Bit 0 und 3 in PORTB
 
 #include <Arduino.h>
+//#include <avr/sleep.h>
 
 #define ledISR PB0
 #define ledPinCIR PB1
@@ -9,6 +10,7 @@
 #define button PB4
 
 int sleepCnt = 0;
+int pinChangeCnt = 0;
 
 bool sleepStatus = false;
 bool pinChangeStatus = false;
@@ -21,6 +23,7 @@ ISR(WDT_vect) { // MUST be present, otherwise ATTiny reboots instead of returnin
 
 ISR(PCINT0_vect)
 {
+  pinChangeCnt = pinChangeCnt + 1;
   pinChangeStatus = true;
 }
 
@@ -37,7 +40,7 @@ void watchDogSetup(void) {
 void pinChangeIntSetup(void){
   SREG &= ~(1 << 7); //entspricht "cli();" - bit7 in SREG wird auf 0 gesetzt
   GIMSK |= (1 << PCIE) & ~(1 << INT0); //Setze des Pin Change Interrupt Enable Bit - loeschen des INT0 Interrupt enable
-  PCMSK |= (1 << PCINT4); //Setzen des Pin Change Enable Mask Bit 4 (PCINT4)  ==> Pin PB4
+  PCMSK |= (1 << PCINT4); //& ~(1 << PCINT5) & ~(1 << PCINT3) & ~(1 << PCINT2) & ~(1 << PCINT1) & ~(1 << PCINT0); //Setzen des Pin Change Enable Mask Bit 4 (PCINT4)  ==> Pin PB4
   SREG |= (1 << 7); //entspricht "sei();" - bit7 in SREG wird auf 1 gesetzt
 }
 
@@ -45,13 +48,15 @@ void sendToSleep(void) {
   byte adcsra;
   adcsra = ADCSRA; // save ADC control and status register A
   ADCSRA &= ~(1 << ADEN); // disable ADC
-
+  PRR |= (1 << PRADC); //| (1 << PRTIM0) | (1 << PRTIM1);
+  //GIMSK &= ~(1 << PCIE); // Pin Change Interrupt AUS im Sleep
   MCUCR |= (1 << SM1) & ~(1 << SM0); // Sleep-Modus = Power Down
   MCUCR |= (1 << SE); // set sleep enable
   //sleep_cpu(); // sleep
   asm("SLEEP"); // sleep_cpu();
   MCUCR &= ~(1 << SE); // reset sleep enable
   MCUSR &= ~(1 << WDRF); // remove reset flag
+  //GIMSK |= (1 << PCIE) & ~(1 << INT0); // Pin Change Interrupt wieder an
   ADCSRA = adcsra; // restore ADC control and status register A
 }
 
@@ -85,8 +90,8 @@ void loop() {
     digitalWrite(ledISR, HIGH);
   }
   if (pinChangeStatus == true) {
-    digitalWrite(ledPinCIR,!digitalRead(ledPinCIR));    
     pinChangeStatus = false;
+    digitalWrite(ledPinCIR,!digitalRead(ledPinCIR));   
   }
   
 }
