4aeff8647af289ade60eda401913004c3e87b7c6
christoph-myC-beginner <christoph.schumachers@gmx.de>
2022-01-30 06:41:15 +0100

    stepper running

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,87 @@
 #include <Arduino.h>
 //#include <avr/sleep.h>
 
-#define ledISR PB0
-#define ledPinCIR PB1
-#define ledStatus PB3
-#define button PB4
+// Include the libraries we need
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS PB4
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+int tempC = 0;
+int tempCOld = 0;
+
+#define NrOfMotorOutputs 4 //Anzahl der AusgÃ¤nge zur Ansteuerung des Motors
+#define NrOfPatternsOneTurn 4 //Anzahl der Muster zur Ansteuerung eines Schritts
+
+//define direction
+#define DirMotorRight 1 
+#define DirMotorLeft 0
+
+byte ActStepNr = 0; //Schritte innerhalb des Step-Patterns
+
+//Schrittmuster definieren
+byte StepPattern[NrOfPatternsOneTurn][NrOfMotorOutputs] = 
+{
+  {1,1,0,0},
+  {0,1,1,0},
+  {0,0,1,1},
+  {1,0,0,1}
+};
+
+//define ports for stepper motor
+byte MotorOutputPin[4] = {0,1,2,3};
+
+//int StepCount = 0;
 
 int sleepCnt = 0;
 int pinChangeCnt = 0;
 
 bool sleepStatus = false;
 bool pinChangeStatus = false;
+bool checkTempStatus = false;
+
+void doStep(bool MotorDir) {
+  if (MotorDir == DirMotorRight) { //Zaehle Positiv - DirMotorRight = 0
+    ActStepNr++;
+    if (ActStepNr == NrOfPatternsOneTurn){ ActStepNr = 0;}
+    for (byte j=0; j < NrOfMotorOutputs; j++)
+    {
+      digitalWrite(MotorOutputPin[j], StepPattern[ActStepNr][j]);
+    }
+  }
+  else {//means (MotorDir == DirMotorLeft) //Zaehle Negativ
+    if (ActStepNr == 0) {ActStepNr = NrOfPatternsOneTurn-1;}
+    else {ActStepNr--;}
+    for (byte j=0; j < NrOfMotorOutputs; j++)
+      {
+        digitalWrite(MotorOutputPin[j], StepPattern[ActStepNr][j]);
+      } 
+  }
+}
+
+void stopMotor(void) {
+  digitalWrite(MotorOutputPin[0], LOW);
+  digitalWrite(MotorOutputPin[1], LOW);
+  digitalWrite(MotorOutputPin[2], LOW);
+  digitalWrite(MotorOutputPin[3], LOW);
+}
 
 ISR(WDT_vect) { // MUST be present, otherwise ATTiny reboots instead of returning from Sleep!
   
   sleepCnt = sleepCnt + 1;
-  sleepStatus = true;
+  //sleepStatus = true;
 } 
 
 ISR(PCINT0_vect)
 {
-  pinChangeCnt = pinChangeCnt + 1;
+  //pinChangeCnt = pinChangeCnt + 1;
   pinChangeStatus = true;
 }
 
@@ -35,6 +96,14 @@ void watchDogSetup(void) {
   WDTCR |= ((1 << WDP0) | (0 << WDP1) | (0 << WDP2) | (1 << WDP3)); // set prescaler bits to to 8s
   WDTCR |= (1 << WDIE); // access WDT interrupt
   SREG |= (1 << 7); //entspricht "sei();" - bit7 in SREG wird auf 1 gesetzt
+}
+
+void WDT_off(void) {
+  MCUSR &= ~(1 << WDRF); // remove reset flag
+  /* Write logical one to WDCE and WDE */
+  WDTCR |= (1<<WDCE) | (1<<WDE);
+  /* Turn off WDT */
+  WDTCR = 0x00;
 }
 
 void pinChangeIntSetup(void){
@@ -53,7 +122,7 @@ void sendToSleep(void) {
   MCUCR |= (1 << SM1) & ~(1 << SM0); // Sleep-Modus = Power Down
   MCUCR |= (1 << SE); // set sleep enable
   //sleep_cpu(); // sleep
-  asm("SLEEP"); // sleep_cpu();
+  asm("SLEEP"); // same as sleep_cpu();
   MCUCR &= ~(1 << SE); // reset sleep enable
   MCUSR &= ~(1 << WDRF); // remove reset flag
   //GIMSK |= (1 << PCIE) & ~(1 << INT0); // Pin Change Interrupt wieder an
@@ -62,36 +131,151 @@ void sendToSleep(void) {
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(ledISR, OUTPUT);
-  pinMode(ledPinCIR, OUTPUT);
-  pinMode(ledStatus, OUTPUT);
-  pinMode(button, INPUT_PULLUP);
+   
+  pinMode(PB0, OUTPUT); //Motor Pin 0
+  pinMode(PB1, OUTPUT); //Motor Pin 1
+  pinMode(PB2, OUTPUT); //Motor Pin 2
+  pinMode(PB3, OUTPUT); //Motor Pin 3
   
+  // Start up the library
+  sensors.begin();
+
+  //WDT_off();
   watchDogSetup();
-  pinChangeIntSetup();
+  //pinChangeIntSetup();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (sleepStatus == true) {
-    for (int i = 0; i <= sleepCnt; i++) {
-      digitalWrite(ledStatus, HIGH);
-      delay(80);
-      digitalWrite(ledStatus, LOW);
-      delay(80);
-    }
     sendToSleep();
   }
-  if (sleepCnt >= 1) {
-    digitalWrite(ledISR,!digitalRead(ledISR));
+  if (sleepCnt >= 3) {
     sleepCnt = 0;
+    for (int i=0; i<1; i++){ 
+      doStep(0);
+      delay(10);
+      stopMotor();
+      delay(50);
+    }
+    for (int i=0; i<1; i++){ 
+      doStep(1);
+      delay(10);
+      stopMotor();
+      delay(50);
+    }
+    for (int i=0; i<1; i++){ 
+      doStep(1);
+      delay(10);
+      stopMotor();
+      delay(50);
+    }
+    for (int i=0; i<1; i++){ 
+      doStep(0);
+      delay(10);
+      stopMotor();
+      delay(50);
+    }
+    checkTempStatus = true;
   }
-  else if (sleepCnt > 4) {
-    digitalWrite(ledISR, HIGH);
+  else {
   }
   if (pinChangeStatus == true) {
     pinChangeStatus = false;
-    digitalWrite(ledPinCIR,!digitalRead(ledPinCIR));   
-  }
+    //GIMSK &= ~(1 << PCIE);
+    //GIFR &= ~((1 << PCIF) | (1 << INTF0));
+
+    //GIMSK |= (1 << PCIE) & ~(1 << INT0); 
+  } 
   
+  if (checkTempStatus == true) {
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    tempC = (sensors.getTempCByIndex(0) + 0.5);
+    
+    if (tempC > tempCOld) {
+      for (int i=0; i<(tempC - tempCOld); i++){ 
+        doStep(DirMotorRight);
+        delay(10);
+        stopMotor();
+        delay(50);
+      }
+      tempCOld = tempC; 
+      sleepStatus = true;
+    }
+    else if (tempC < tempCOld) {
+      for (int i=0; i<(tempCOld - tempC); i++){ 
+        doStep(DirMotorLeft);
+        delay(10);
+        stopMotor();
+        delay(50);
+      }
+      tempCOld = tempC;
+      sleepStatus = true;
+    }
+    else {
+      tempCOld = tempC;
+    }
+    checkTempStatus = false;
+  }
+  else {
+    sleepStatus = true;
+  }
+  /*
+  delay(1000);
+  for (int i=0; i<20; i++){ 
+    doStep(0);
+    delay(300);
+    stopMotor();
+  }
+  //stopMotor();
+  delay(1000);
+  for (int i=0; i<20; i++){ 
+    doStep(1);
+    delay(3);
+    stopMotor();
+  }
+  //stopMotor();
+  digitalWrite(PB0, HIGH);
+  digitalWrite(PB1, HIGH);
+  digitalWrite(PB2, LOW);
+  digitalWrite(PB3, LOW);
+  delay(30);
+  digitalWrite(PB0, LOW);
+  digitalWrite(PB1, LOW);
+  digitalWrite(PB2, LOW);
+  digitalWrite(PB3, LOW);
+  delay(500);
+  digitalWrite(PB0, LOW);
+  digitalWrite(PB1, HIGH);
+  digitalWrite(PB2, HIGH);
+  digitalWrite(PB3, LOW);
+  delay(30);
+  digitalWrite(PB0, LOW);
+  digitalWrite(PB1, LOW);
+  digitalWrite(PB2, LOW);
+  digitalWrite(PB3, LOW);
+  delay(500);
+  digitalWrite(PB0, LOW);
+  digitalWrite(PB1, LOW);
+  digitalWrite(PB2, HIGH);
+  digitalWrite(PB3, HIGH);
+  delay(30);
+  digitalWrite(PB0, LOW);
+  digitalWrite(PB1, LOW);
+  digitalWrite(PB2, LOW);
+  digitalWrite(PB3, LOW);
+  delay(500);
+  digitalWrite(PB0, HIGH);
+  digitalWrite(PB1, LOW);
+  digitalWrite(PB2, LOW);
+  digitalWrite(PB3, HIGH);
+  delay(30);
+  digitalWrite(PB0, LOW);
+  digitalWrite(PB1, LOW);
+  digitalWrite(PB2, LOW);
+  digitalWrite(PB3, LOW);
+  delay(500);
+  */
+  //stopMotor();
+
 }
